1c32e9117e57287a7668f7c4fcd588af0ccca212
Johan Glud Jensen <johancentralen@gmail.com>
2025-04-02 21:47:17 +0200

    setup vitest config and gh flow

---
+++ src/try-catch.test.ts
@@ -0,0 +1,10 @@
+import { describe, test } from 'vitest';
+
+describe('try-catch', () => {
+    test.skip('should have successfully retrieved data from fetch', () => {});
+    test.skip('should have error not be null from fetch', () => {});
+
+    test.skip('should have successfully resolved promise', () => {});
+    test.skip('should have error not be null from rejected promise', () => {});
+});
+
