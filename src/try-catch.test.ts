bf3411b23ee8936ad7b100ef1cfc7db7853e5cf6
Johan Glud Jensen <johancentralen@gmail.com>
2025-04-04 20:36:49 +0200

    test: write tests for fetch and promise usecases

--- src/try-catch.test.ts
+++ src/try-catch.test.ts
@@ -1,10 +1,46 @@
-import { describe, test } from 'vitest';
+import { vi, describe, test, expect } from "vitest";
+import { tryCatch } from "./try-catch";
 
-describe('try-catch', () => {
-    test.skip('should have successfully retrieved data from fetch', () => {});
-    test.skip('should have error not be null from fetch', () => {});
+const RESPONSE = {
+  FAILURE: 'failure',
+  SUCCESS: 'success'
+} as const;
+const { SUCCESS, FAILURE } = RESPONSE;
 
-    test.skip('should have successfully resolved promise', () => {});
-    test.skip('should have error not be null from rejected promise', () => {});
+describe("try-catch", () => {
+  test("should have successfully retrieved data from fetch", async () => {
+    vi.stubGlobal('fetch', vi.fn(() =>
+      Promise.resolve({ ok: true, json: () => Promise.resolve(SUCCESS)}),
+    ));
+
+    const { data, error } = await tryCatch(fetch('http://random-test-url.org').then(res => res.json()));
+
+    expect(error).toBeNull();
+    expect(data).toBe(SUCCESS);
+  });
+
+  test("should have error not be null from fetch", async () => {
+    vi.stubGlobal('fetch', vi.fn(() =>
+      Promise.resolve({ ok: false, json: () => Promise.reject(FAILURE)}),
+    ));
+
+    const { data, error } = await tryCatch(fetch('http://random-test-url.org').then(res => res.json()));
+
+    expect(error).toBe(FAILURE);
+    expect(data).toBeNull();
+  });
+
+  test("should have successfully resolved promise", async () => {
+    const { data, error } = await tryCatch(Promise.resolve(Promise.resolve(SUCCESS)));
+
+    expect(error).toBeNull();
+    expect(data).toBe(SUCCESS);
+  });
+
+  test("should have error not be null from rejected promise", async () => {
+    const { data, error } = await tryCatch(Promise.resolve(Promise.reject(FAILURE)));
+
+    expect(error).toBe(FAILURE);
+    expect(data).toBeNull();
+  });
 });
-
